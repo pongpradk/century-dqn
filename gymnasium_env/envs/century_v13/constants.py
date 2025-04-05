58bfa363ab6a66802c25e4d517b16d6735ce5f4b
pongpradk <pongpradk@gmail.com>
2025-04-05 17:31:35 +0700

    Update 'GOLEM_ENDGAME_THRESHOLD' to 5

--- gymnasium_env/envs/century_v13/constants.py
+++ gymnasium_env/envs/century_v13/constants.py
@@ -1,6 +1,6 @@
 GAME_CONSTANTS = {
     'MAX_CRYSTALS': 10,
-    'GOLEM_ENDGAME_THRESHOLD': 3,
+    'GOLEM_ENDGAME_THRESHOLD': 5,
     'CRYSTAL_VALUES': {
         'yellow': 0.3,
         'green': 1.0,
